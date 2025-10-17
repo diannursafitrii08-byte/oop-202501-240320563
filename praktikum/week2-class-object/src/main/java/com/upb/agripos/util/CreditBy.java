a02766981e0d8274c7aee14b583c761cd13d7947
Dian Nur Safitri <diannursafitrii08@gmail.com>
2025-10-17 17:44:33 +0700

    week2-class-object

---
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/util/CreditBy.java
@@ -0,0 +1,9 @@
+package util;
+
+public class CreditBy {
+    public static void print(String nama, String nim) {
+        System.out.println("\n=== Data Mahasiswa ===");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM : " + nim);
+    }
+}
