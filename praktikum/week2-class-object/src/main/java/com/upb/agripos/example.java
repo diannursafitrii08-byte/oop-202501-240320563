5ac8501f44600eec2e8684602c649a9f73bb0c05
Dian Nur Safitri <diannursafitrii08@gmail.com>
2025-10-27 10:45:29 +0700

    week3-inheritance

---
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/example.java
@@ -0,0 +1,5 @@
+package src.main.java.com.upb.agripos;
+
+public class example {
+    
+}
--- praktikum/week2-class-object/src/main/java/com/upb/agripos/util/CreditBy.java
+++ praktikum/week2-class-object/src/main/java/com/upb/agripos/util/CreditBy.java
@@ -4,6 +4,6 @@ public class CreditBy {
     public static void print(String nama, String nim) {
         System.out.println("\n=== Data Mahasiswa ===");
         System.out.println("Nama: " + nama);
-        System.out.println("NIM : " + nim);
-    }
+        System.out.println("NIM : " + nim);
+    }
 }
